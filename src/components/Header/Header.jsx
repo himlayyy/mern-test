bcbdfae9338ae53f091e9b5764a42cb27c197b4c
himlayyy <alesandralelis@gmail.com>
2022-06-04 23:26:24 +0800

    Created header components & added FontAwesome icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import "./header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
     <>
-        <div className="header">
-            <div className="headerList">
-                <div className="headerListItem">
-                    
-                </div>
-            </div>
+      <div className="header">
+        <div className="headerList">
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={"bed"} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={"plane"} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={"car"} />
+            <span>Car rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={"mountain"} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={"car"} />
+            <span>Airport taxis</span>
+          </div>
         </div>
+      </div>
     </>
-
-  )
+  );
 }
 
-export default Header
+export default Header;
