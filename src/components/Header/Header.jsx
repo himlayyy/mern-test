2e372fba044c44d95048f94e87a8f711fa031276
himlayyy <alesandralelis@gmail.com>
2022-06-04 23:57:39 +0800

    Added header description and title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,27 +6,35 @@ function Header() {
   return (
     <>
       <div className="header">
-        <div className="headerList">
-          <div className="headerListItem">
-            <FontAwesomeIcon icon={"bed"} />
-            <span>Stays</span>
+        <div className="headerContainer">
+          <div className="headerList">
+            <div className="headerListItem active">
+              <FontAwesomeIcon icon={"bed"} />
+              <span>Stays</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={"plane"} />
+              <span>Flights</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={"car"} />
+              <span>Car rentals</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={"mountain"} />
+              <span>Attractions</span>
+            </div>
+            <div className="headerListItem">
+              <FontAwesomeIcon icon={"car"} />
+              <span>Airport taxis</span>
+            </div>
           </div>
-          <div className="headerListItem">
-            <FontAwesomeIcon icon={"plane"} />
-            <span>Flights</span>
-          </div>
-          <div className="headerListItem">
-            <FontAwesomeIcon icon={"car"} />
-            <span>Car rentals</span>
-          </div>
-          <div className="headerListItem">
-            <FontAwesomeIcon icon={"mountain"} />
-            <span>Attractions</span>
-          </div>
-          <div className="headerListItem">
-            <FontAwesomeIcon icon={"car"} />
-            <span>Airport taxis</span>
-          </div>
+          <h1 className="headerTitle">A lifetime of discounts? It's Genius</h1>
+          <p className="headerDesc">
+            Get rewared for your travels - unlock instant savings of 10% or more
+            with a free Ashbook account
+          </p>
+          <button className="headerBtn">Sign in / Register</button>
         </div>
       </div>
     </>
