aad3cd07b6e7ce584a80caf32e9f6b8ffee4d5fd
himlayyy <alesandralelis@gmail.com>
2022-06-06 12:12:17 +0800

    Rendered Featured component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import Featured from '../../components/Featured/Featured';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Home/Navbar';
+import "./home.css"
 
 function Home() {
   return (
     <>
       <Navbar/>
       <Header/>
+      <div className="homeContainer">
+        <Featured />
+      </div>
     </>
   )
 }
