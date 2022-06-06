eaefa28d69cd8af7b90a8f2b035299bc5341fe69
himlayyy <alesandralelis@gmail.com>
2022-06-06 14:40:57 +0800

    Added MailList component

--- src/components/MailList/MailList.jsx
+++ src/components/MailList/MailList.jsx
@@ -3,7 +3,14 @@ import "./maillist.css"
 
 function MailList() {
   return (
-    <div>MailList</div>
+    <div className="mail">
+        <h1 className="mailTitle">Save time, save money!</h1>
+        <span className="mailDesc">Sign up and we'll send deals to you</span>
+        <div className="mailInputContainer">
+            <input type="text" name="email" id="email" placeholder="Your Email" />
+            <button>Subscribe</button>
+        </div>
+    </div>
   )
 }
 
