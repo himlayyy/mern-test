f85047ed5e6b54d905055bfffe7d76417a2dbda2
himlayyy <alesandralelis@gmail.com>
2022-06-06 13:10:21 +0800

    Fixed Navbar import path

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Featured from '../../components/Featured/Featured';
 import Header from '../../components/Header/Header';
-import Navbar from '../../components/Home/Navbar';
+import Navbar from '../../components/Navbar/Navbar';
 import "./home.css"
 
 function Home() {
