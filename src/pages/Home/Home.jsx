12b1999f2b0703fef39a2d90cb8ea782229c6a91
himlayyy <alesandralelis@gmail.com>
2022-06-06 14:40:18 +0800

    Rendered MailList component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from '../../components/Featured/Featured';
 import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties';
 import Header from '../../components/Header/Header';
+import MailList from '../../components/MailList/MailList';
 import Navbar from '../../components/Navbar/Navbar';
 import PropertyList from '../../components/PropertyList/PropertyList';
 import "./home.css"
@@ -17,6 +18,7 @@ function Home() {
         <PropertyList/>
         <h2 className="homeTitle">Homes guests love</h2>
         <FeaturedProperties />
+        <MailList/>
       </div>
     </>
   )
