2a85984e64ee54eb0300d06667f7b9ca85a23caa
himlayyy <alesandralelis@gmail.com>
2022-06-07 11:34:40 +0800

    Added Search bar on Hotels page

--- src/pages/Hotels/Hotels.jsx
+++ src/pages/Hotels/Hotels.jsx
@@ -1,17 +1,95 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Navbar from '../../components/Navbar/Navbar'
-
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Header from "../../components/Header/Header";
+import Navbar from "../../components/Navbar/Navbar";
+import "./hotels.css";
+import { format } from "date-fns";
+import { DateRange } from "react-date-range";
 
 function Hotels() {
+  const location = useLocation();
+  const [destination, setDestination] = useState(location.state.destination);
+  const [date, setDate] = useState(location.state.date);
+  const [options, setOptions] = useState(location.state.options);
+
+  const [openDate, setOpenDate] = useState(false);
+
   return (
     <>
-    <Navbar/>
-    <Header type="list"/>
-
+      <Navbar />
+      <Header type="list" />
+      <div className="listContainer">
+        <div className="listWrapper">
+          <div className="listSearch">
+            <h1 className="listTitle">Search</h1>
+            <div className="listItem">
+              <label>Destination</label>
+              <input type="text" placeholder={destination} />
+            </div>
+            <div className="listItem">
+              <label>Check-in Date</label>
+              <span onClick={() => setOpenDate(!openDate)}>{`${format(
+                date[0].startDate,
+                "MM/dd/yy"
+              )} to ${format(date[0].endDate, "MM/dd/yy")}`}</span>
+              {openDate && (
+                <DateRange
+                  onChange={(item) => setDate([item.selection])}
+                  minDate={new Date()}
+                  ranges={date}
+                />
+              )}
+            </div>
+            <div className="listItem">
+              <label>Options</label>
+              <div className="listOptions">
+                <div className="listOptionItem">
+                  <span className="listOptionText">
+                    Min price<small>per night</small>
+                  </span>
+                  <input type="number" className="listOptionInput" />
+                </div>
+                <div className="listOptionItem">
+                  <span className="listOptionText">
+                    Max price<small>per night</small>
+                  </span>
+                  <input type="number" className="listOptionInput" min={1} />
+                </div>
+                <div className="listOptionItem">
+                  <span className="listOptionText">Adult</span>
+                  <input
+                    type="number"
+                    className="listOptionInput"
+                    placeholder={options.adult}
+                  />
+                </div>
+                <div className="listOptionItem">
+                  <span className="listOptionText">Children</span>
+                  <input
+                    type="number"
+                    className="listOptionInput"
+                    min={0}
+                    placeholder={options.children}
+                  />
+                </div>
+                <div className="listOptionItem">
+                  <span className="listOptionText">Room</span>
+                  <input
+                    type="number"
+                    className="listOptionInput"
+                    min={1}
+                    placeholder={options.room}
+                  />
+                </div>
+              </div>
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="listResult"></div>
+        </div>
+      </div>
     </>
-    
-  )
+  );
 }
 
-export default Hotels
+export default Hotels;
