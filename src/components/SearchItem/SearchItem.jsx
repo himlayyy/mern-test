fa40e022541a67cf8286611aafb0b8aa3ef88511
himlayyy <alesandralelis@gmail.com>
2022-06-16 00:17:40 +0800

    Added useFetch & useFetch.reFetch to create a request URL based on the search params

--- src/components/SearchItem/SearchItem.jsx
+++ src/components/SearchItem/SearchItem.jsx
@@ -1,38 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./searchitem.css";
 
-function SearchItem() {
+function SearchItem({item}) {
   return (
     <div className="searchItem">
       <img
-        src="https://cf.bstatic.com/xdata/images/hotel/square200/123731236.webp?k=ab2cc311beb4c94c6860e0cbb5453b0df7884d691e596fd8f07d3beea071f51d&o=&s=1"
-        alt="MOXY NYC Times Square"
+        src={item.photos[0]}
+        alt={item.title}
         className="siImg"
       />
       <div className="siDesc">
-        <h3 className="siTitle">MOXY NYC Times Square</h3>
-        <span className="siDistance">1.9 km from center</span>
+        <h3 className="siTitle">{item.title}</h3>
+        <span className="siDistance">{item.distance}</span>
         <span className="siTaxiOp">Free airport taxi</span>
         <span className="siSubtitle">
           MOXY NYC Times Square, offers pet-friendly accommodations, and
           features free WiFi and a fitness center. Macy's is 656 feet from the
           property.
         </span>
-        <span className="siFeatures">1 bathroom, 1 full bed</span>
+        <span className="siFeatures">{item.desc}</span>
         <span className="siCancelOp">Free cancellation</span>
         <span className="siCancelOpSubtitle">
           You can cancel later, so lock in this great price today
         </span>
       </div>
       <div className="siDetails">
+        {item.rating &&
         <div className="siRating">
           <span>Excellent</span>
-          <button>8.9</button>
+          <button>{item.rating}</button>
         </div>
+        }
         <div className="siDetailsText">
-          <span className="siPrice">$123</span>
+          <span className="siPrice">₱ {item.cheapestPrice}</span>
           <span className="siTaxOp">Includes taxes and fees</span>
+          <Link to={`/hotel/${item._id}`}>
           <button className="siCheckButton">See availability</button>
+          </Link>
         </div>
       </div>
     </div>
