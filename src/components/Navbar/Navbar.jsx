89025a8e727200a41fe6ba26262a9ba502c7ce91
himlayyy <alesandralelis@gmail.com>
2022-06-15 22:11:40 +0800

    Fixed Link to

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import "./navbar.css"
+import React from "react";
+import "./navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
-        <div className="navBar">
-            <div className="navContainer">
-                <span className="logo">Booking.com</span>
-                <div className="navItems">
-                    <button className="navButton">Register</button>
-                    <button className="navButton">Login</button></div>
-            </div>
+      <div className="navBar">
+        <div className="navContainer">
+          <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+            <span className="logo">Booking.com</span>
+          </Link>
+          <div className="navItems">
+            <button className="navButton">Register</button>
+            <button className="navButton">Login</button>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
